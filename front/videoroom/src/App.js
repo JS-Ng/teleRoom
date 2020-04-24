0d7341766ed5d5d5dcb1764f3d9e796bd811b386
junxuan <atrocitythemetms@gmail.com>
2020-04-24 02:09:55 -0400

    populate video

--- front/videoroom/src/App.js
+++ front/videoroom/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ReactPlayer from 'react-player'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" />  */}
+        <div className="Video">
+          <ReactPlayer url="https://youtu.be/RTvk9KmLcms" controls="true" playing ></ReactPlayer>
+        </div>       
       </header>
     </div>
   );
---
+++ heroku.sh
@@ -0,0 +1,35 @@
+# base function to split the array
+split(){
+  cur_temp=$IFS
+  IFS="="
+  read -ra spt_array <<< $1
+  IFS=$cur_temp
+  local -n arr=$2
+  arr=(${spt_array[*]})
+}
+# checker 1
+checker_version(){
+  local my_arr
+  split $1 my_arr
+  if [ ${my_arr[0]} = "version" ]; then
+    echo 0
+  else
+    echo 1
+  fi
+}
+# find property
+read_property(){
+  file_name=$1
+  while read -r line
+  do
+    if [ "$(checker_version $line)" = "0" ]; then
+      echo $line
+    fi
+  done < <(grep . $file_name)
+}
+
+for i in $(split "123=456"); do
+  echo $i
+done
+
+checker_version "version=456"
